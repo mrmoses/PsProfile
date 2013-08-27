27e85a50713b808abb618de49c1a5508fb5f4b9d
Jason J. Rotello <jason@jrotello.com>
2013-08-26 23:18:59 -0500

    Fixed profile load performance issue.

--- helpers.ps1
+++ helpers.ps1
@@ -1,11 +1,11 @@
-function Get-IsSessionElevated {   
+function Get-IsSessionElevated {
     [System.Security.Principal.WindowsPrincipal]$currentPrincipal = `
         New-Object System.Security.Principal.WindowsPrincipal(
         [System.Security.Principal.WindowsIdentity]::GetCurrent());
 
     [System.Security.Principal.WindowsBuiltInRole]$administratorsRole = `
         [System.Security.Principal.WindowsBuiltInRole]::Administrator;
-    
+
     if($currentPrincipal.IsInRole($administratorsRole)) {
         return $true;
     } else {
@@ -13,8 +13,9 @@ function Get-IsSessionElevated {
     }
 }
 
+
 function Test-Module($name) {
-    return (Get-Module -ListAvailable | ? Name -eq $name) -ne $null
+    return Get-Module -ListAvailable -Name $name
 }
 
 function ImportOrPsGet-Module($name) {
@@ -27,22 +28,20 @@ function ImportOrPsGet-Module($name) {
             Invoke-RestMethod "http://psget.net/GetPsGet.ps1" | iex
         } else {
             Install-Module $name
-        }        
+        }
     }
 }
 
 function Initialize-TfsPowerTools() {
     $tfsPowerToolsPath = $Env:TFSPowerToolDir
     if ($tfsPowerToolsPath -eq $null) {
-        $tfsPowerToolsPath = "C:\Program Files (x86)\Microsoft Team Foundation Server 2012 Power Tools"   
+        $tfsPowerToolsPath = "C:\Program Files (x86)\Microsoft Team Foundation Server 2012 Power Tools"
     }
 
     if (Test-Path $tfsPowerToolsPath) {
         Add-PSSnapin Microsoft.TeamFoundation.PowerShell
-        . "$tfsPowerToolsPath\TFSSnapin.ps1"
-
         Write-Host "TFS 2012 Power Tools Initialized" -ForegroundColor Green
-    }   
+    }
 }
 
 function Initialize-PoshGit() {
@@ -87,23 +86,23 @@ function Test-Url() {
     Process {
         foreach($u in $url) {
             try {
-                Invoke-WebRequest $u |                     
+                Invoke-WebRequest $u |
                     select StatusCode, StatusDescription |
                     Add-Member -MemberType NoteProperty -Name URL -Value $u -PassThru
-                    
+
             } catch {
                 $ex = $_.Exception
-                if ($ex.Status -eq [System.Net.WebExceptionStatus]::ProtocolError) {            
+                if ($ex.Status -eq [System.Net.WebExceptionStatus]::ProtocolError) {
                      $obj = New-Object -TypeName psobject |
                         Add-Member -MemberType NoteProperty -Name StatusCode -Value ($ex.Response.StatusCode -as [int]) -PassThru |
                         Add-Member -MemberType NoteProperty -Name StatusDescription -Value $ex.Response.StatusDescription -PassThru |
                         Add-Member -MemberType NoteProperty -Name URL -Value $u -PassThru
-                        
+
                         Write-Output $obj
                 } else {
                     Write-Host $ex.Message -ForegroundColor Red
                 }
-            }            
+            }
         }
     }
 }
