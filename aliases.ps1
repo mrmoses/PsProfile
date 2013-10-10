dfcd7536106502cefcd152b5c031f048237c92ff
Jason J. Rotello <jason@jrotello.com>
2013-10-10 08:53:05 -0500

    Added alias for Java keytool.exe

--- aliases.ps1
+++ aliases.ps1
@@ -1,2 +1,3 @@
 Set-Alias sublime "C:\Program Files\Sublime Text 2\sublime_text.exe"
+Set-Alias keytool "C:\Program Files\Java\jre7\bin\keytool.exe"
 Set-Alias Out-Clipboard "$env:SystemRoot\system32\clip.exe"
