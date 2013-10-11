ad4d3f80ad2e991faddc904b6581944237114e10
Jason J. Rotello <jason@jrotello.com>
2013-10-10 21:21:46 -0500

    Added alias for curl.exe if it is installed via chocolatey

--- aliases.ps1
+++ aliases.ps1
@@ -1,3 +1,8 @@
 Set-Alias sublime "C:\Program Files\Sublime Text 2\sublime_text.exe"
 Set-Alias keytool "C:\Program Files\Java\jre7\bin\keytool.exe"
+
+if (Test-Path "c:\Chocolatey\bin\curl.bat") {
+    Set-Alias curl "c:\Chocolatey\bin\curl.bat" -Force -Option AllScope
+}
+
 Set-Alias Out-Clipboard "$env:SystemRoot\system32\clip.exe"
